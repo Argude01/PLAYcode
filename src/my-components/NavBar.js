2b6aef68473685754dd00c9f0833936b6e275f61
Argude01 <30556136+Argude01@users.noreply.github.com>
2019-04-13 17:22:10 -0600

    NavBar v0.1.0

--- src/my-components/NavBar.js
+++ src/my-components/NavBar.js
@@ -1,81 +1,62 @@
 import React from 'react';
 import '../css/NavBar.css';
-import logo from '../img/icon-playcode.png';
-import searchIcon from '../icons-svg/baseline-search-24px.svg';
-import helpIcon from '../icons-svg/baseline-help_outline-24px.svg';
-import settingIcon from '../icons-svg/round-settings-24px.svg';
-import appsIcon from '../icons-svg/round-apps-24px.svg';
-import userIcon from '../icons-svg/user.svg';
-import { Container } from 'react-bootstrap';
+import logo from '../img/svg/PLAYcode-navbar-md.svg';
+import searchIcon from '../img/icons-svg/baseline-search.svg';
+import helpIcon from '../img/icons-svg/baseline-help_outline.svg';
+import settingIcon from '../img/icons-svg/round-settings.svg';
+import appsIcon from '../img/icons-svg/round-apps.svg';
+import userIcon from '../img/icons-svg/user.svg';
 
 export class NavBar extends React.Component {
     render() {
         return (
-            <Container fluid>                
-                <nav className="navbar navbar-expand-sm">                    
-                    <div className="navbar-header">
-                        <a className="navbar-brand" href="#">
-                            <img src={logo} alt="PLAYcode logo"></img>
+            <div class="my-navbar">
+                <nav className="navbar navbar-expand-sm justify-content-center">
+                    <div className="container-fluid" style={{marginLeft: '0.5rem'}}>
+                        <a class="navbar-brand" href="#">
+                            <img src={logo} alt="logo-PLAYcode" />
                         </a>
                     </div>
-                    <div style={{ marginLeft: '15rem' }}>    
-                        <form className="form-inline">
-                            <div className="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" id="basic-addon1">
-                                        <img src={searchIcon} alt="Search"></img>
-                                    </span>
+                    <div className="container-fluid">                        
+                        <form className="form-inline" action="">
+                            <div className="input-group" style={{marginLeft: '16rem'}}>
+                                <div className="input-group-prepend" style={{paddingRight: '1rem'}}>
+                                    <img className="" src={searchIcon} alt="search icon"/>
                                 </div>
-                                <input type="text" className="form-control" placeholder="Buscar en PLAYcode                    " />
-                            </div>     
+                                <input type="text" className="form-control search-input" placeholder="Buscar en PLAYcode"/>
+                            </div>
                         </form>
                     </div>
-                    <div style={{ marginLeft: '10rem' }}>
-                        <ul className="nav navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link">
-                                    <img src={helpIcon}></img>
+                    <div className="container-fluid">
+                        <ul className="navbar-nav">
+                            <li className="navbar-item" style={{marginRight: '2rem', marginLeft: '8rem'}}>
+                                <a href="#" className="navbar-link">
+                                    <img src={helpIcon} alt="help icon"/>
                                 </a>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link">
-                                    <img src={settingIcon}></img>
+                            <li className="navbar-item" style={{marginRight: '2rem'}}>
+                                <a href="#" className="navbar-link">
+                                    <img src={settingIcon} alt="setting icon"/>
+                                </a>
+                            </li>
+                            <li className="navbar-item" style={{marginRight: '2rem'}}>
+                                <a href="#" className="navbar-link">
+                                    <img src={appsIcon} alt="apps icon"/>
+                                </a>
+                            </li>
+                            <li className="navbar-item">
+                                <a href="#" className="navbar-link">
                                 </a>
                             </li>
                         </ul>
                     </div>
-                    <div style={{ marginLeft: '5rem' }}>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li className="nav-item">
-                                <a className="nav-link">
-                                    <img src={appsIcon}></img>
-                                </a>
-                            </li> 
-                            <li className="nav-item">
-                                <a className="nav-link">
-                                    <div className="circular">
-                                        <img src={userIcon}></img>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>    
-                    </div>                    
+                    <div className="container-fluid">
+                        <div className="circular">
+                            <img src={userIcon} alt="user icon"/>
+                        </div>
+                    </div>
                 </nav>
-                
-            </Container>
-
-/*
-            <nav className="navbar">
-                <a className="navbar-brand" href="#">
-                    <img src={logo} alt="logo"/>
-                </a>
-                <form class="form-inline">
-                    <input class="form-control" type="text" placeholder="Search" name="search-box" />
-                    <input class="btn btn-secondary" type="button" value="Search" />
-                </form>
-                   
-                            
-            </nav>*/
+            </div>
         );
     }
 }
